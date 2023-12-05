9396bde30d581f1fb1feffc1268f6c7c7296938a
Murilo Barbosa <muriloab96@gmail.com>
2023-12-05 11:14:21 -0300

    Created the TicketHistory class

---
+++ Models/TicketHistory.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace BugOut.Models
+{
+    public class TicketHistory
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; } //Foreign key
+
+        [DisplayName("Updated Item")]
+        public string? Property { get; set; } //Ex: description, status, etc...
+
+        [DisplayName("Previous")]
+        public string? OldValue { get; set; }
+
+        [DisplayName("Current")]
+        public string? NewValue { get; set; }
+
+        [DisplayName("Date Modified")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Description of Change")]
+        public string? Description { get; set; }
+
+        [DisplayName("Team Member")]
+        public string? UserId { get; set; } //Foreign Key
+
+        //Navigation Properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual AppUser? User { get; set; }
+
+    }
+}
