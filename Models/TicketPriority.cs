60099078e31542134c9442673b816efd67f75202
Murilo Barbosa <muriloab96@gmail.com>
2023-12-05 10:45:07 -0300

    Created data amodels for ticket priority, status, and type

---
+++ Models/TicketPriority.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace BugOut.Models
+{
+    public class TicketPriority
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Priority Name")]
+        public string? Name { get; set; }
+
+    }
+}
---
+++ Models/TicketType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace BugOut.Models
+{
+    public class TicketType
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Type Name")]
+        public string? Name { get; set; }
+
+
+
+    }
+}
