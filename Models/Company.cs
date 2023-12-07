126c3174e8e15695caac14b8199b2080907892d9
Murilo Barbosa <muriloab96@gmail.com>
2023-12-07 15:40:01 -0300

    Created Notification Model

--- Models/Company.cs
+++ Models/Company.cs
@@ -18,6 +18,7 @@ namespace BugOut.Models
         public virtual ICollection<AppUser>? Members { get; set; } = new HashSet<AppUser>();
         public virtual ICollection<Project>? Projects { get; set; } = new HashSet<Project>();
 
+        //Create Relationship to Invite
 
     }
 }
---
+++ Models/Notification.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugOut.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int? TicketId { get; set; }
+
+        [Required]
+        [DisplayName("Title")]
+        public string? Title { get; set; }
+
+        [Required]
+        [DisplayName("Message")]
+        public string? Message { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date")]
+        public DateTimeOffset? Created {  get; set; }
+
+        [Required]
+        [DisplayName("Recipient")]
+        public string? RecipientId { get; set; }
+
+        [Required]
+        [DisplayName("Sender")]
+        public string? SenderId { get; set; }
+
+        [DisplayName("Visualized")]
+        public bool Viewed { get; set; }
+
+        //Navigation Properties
+        public virtual Ticket? Ticket { get; set; }
+        public virtual AppUser? Recipient {  get; set; }
+        public virtual AppUser? Sender { get; set; }
+
+
+
+    }
+}
