7a3c32aa323c57763a4fa0678c877c3763f92129
Murilo Barbosa <muriloab96@gmail.com>
2023-12-08 15:35:26 -0300

    Implemented the IAppRolesService intreface and created the AddUserToRoleAsync method.

---
+++ Services/AppRolesService.cs
@@ -0,0 +1,63 @@
+﻿using BugOut.Data;
+using BugOut.Models;
+using BugOut.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace BugOut.Services
+{
+    public class AppRolesService : IAppRolesService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AppRolesService(ApplicationDbContext context, RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<bool> AddUserToRoleAsync(AppUser user, string roleName)
+        {
+            bool result = (await _userManager.AddToRoleAsync(user, roleName)).Succeeded;
+
+            return result;
+        }
+
+        public Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AppUser>> GetUsersInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AppUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserInRoleAsync(AppUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(AppUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRolesAsync(AppUser user, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
