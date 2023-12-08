097931b578dede99f8f7d263d231129802c4445c
Murilo Barbosa <muriloab96@gmail.com>
2023-12-08 14:58:18 -0300

    created the AppRoleService interface

---
+++ Services/Interfaces/IAppRolesService.cs
@@ -0,0 +1,18 @@
+﻿using BugOut.Models;
+using Microsoft.Identity.Client;
+
+namespace BugOut.Services.Interfaces
+{
+    public interface IAppRolesService
+    {
+        public Task<bool> IsUserInRoleAsync (AppUser user, string roleName);
+        public Task<IEnumerable<string>> GetUserRolesAsync(AppUser user);
+        public Task<bool> AddUserToRoleAsync (AppUser user, string roleName);
+        public Task<bool> RemoveUserFromRoleAsync(AppUser user, string roleName);
+        public Task<bool> RemoveUserFromRolesAsync (AppUser user, IEnumerable<string> roles);
+        public Task<List<AppUser>> GetUsersInRoleAsync(string roleName, int companyId);
+        public Task<List<AppUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+
+    }
+}
