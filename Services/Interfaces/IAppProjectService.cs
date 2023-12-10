0eaa58310f5a9e7114491ff6385823689a814afd
Murilo Barbosa <muriloab96@gmail.com>
2023-12-10 10:53:07 -0300

    Created IAppProjectService interface

---
+++ Services/Interfaces/IAppProjectService.cs
@@ -0,0 +1,30 @@
+﻿using BugOut.Models;
+
+namespace BugOut.Services.Interfaces
+{
+    public interface IAppProjectService
+    {
+        public Task AddNewProjectAsync(Project project);
+        public Task<bool> AddProjectManagerAsync(string userId, int projectId);
+        public Task<bool> AddUserToProjectAsync(string userId, int projectId);
+        public Task ArchiveProjectAsync(Project project);
+        public Task<List<Project>> GettAllProjectsByCompany(int companyId);
+        public Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName);
+        public Task<List<AppUser>> GetAllProjectMembersExceptPMAsync(int projectId);
+        public Task<List<Project>> GetArchivedProjectsByCompany(int companyId);
+        public Task<List<AppUser>> GetDevelopersOnProjectAsync(int projectId);
+        public Task<AppUser>GetProjectManagerASync(int projectId);
+        public Task<List<AppUser>> GetProjectMembersByRoleAsync(int projectId, string role);
+        public Task<Project> GetProjectByIdAsync(int projectId, int companyId);
+        public Task<List<AppUser>> GetSubmittersOnProjectAsync(int projectId);
+        public Task<List<AppUser>> GetUsersNotOnProjectAsync(int projectId, int comopanyId);
+        public Task<List<Project>>GetUserProjectsAsync(string userId);
+        public Task<bool> IsUserOnProject(string userId, int projectId);
+        public Task<int> LookupProjectPriorityId(string priorityName);
+        public Task RemoveProjectManagerAsync(int projectId);
+        public Task RemoveUsersFromProjectByRoleAsync(string role, int projectId);
+        public Task RemoveUserFromProjectAsync(string userId, int projectId);
+        public Task UpdateProjectAsync(Project project);
+
+    }
+}
