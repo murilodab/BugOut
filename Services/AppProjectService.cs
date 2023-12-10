4014c5ad849c23c8433826f78835c7e46958c735
Murilo Barbosa <muriloab96@gmail.com>
2023-12-10 10:54:49 -0300

    created the implementation class for IAppProjectInfeterface. Still need to write the methods

---
+++ Services/AppProjectService.cs
@@ -0,0 +1,113 @@
+﻿using BugOut.Models;
+using BugOut.Services.Interfaces;
+
+namespace BugOut.Services
+{
+    public class AppProjectService : IAppProjectService
+    {
+        public Task AddNewProjectAsync(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddProjectManagerAsync(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddUserToProjectAsync(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ArchiveProjectAsync(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AppUser>> GetAllProjectMembersExceptPMAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AppUser>> GetDevelopersOnProjectAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Project> GetProjectByIdAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> GetProjectManagerASync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AppUser>> GetProjectMembersByRoleAsync(int projectId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AppUser>> GetSubmittersOnProjectAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Project>> GettAllProjectsByCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Project>> GetUserProjectsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AppUser>> GetUsersNotOnProjectAsync(int projectId, int comopanyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserOnProject(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> LookupProjectPriorityId(string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveProjectManagerAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveUserFromProjectAsync(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveUsersFromProjectByRoleAsync(string role, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProjectAsync(Project project)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
