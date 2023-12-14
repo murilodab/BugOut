9c5004ad7634e98f809c7e80a1bddb03d8ecfdf3
Murilo Barbosa <muriloab96@gmail.com>
2023-12-14 13:42:55 -0300

    created IAppInviteService interface

---
+++ Services/Interfaces/IAppInviteService.cs
@@ -0,0 +1,14 @@
+﻿using BugOut.Models;
+
+namespace BugOut.Services.Interfaces
+{
+    public interface IAppInviteService
+    {
+        public Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId);
+        public Task AddNewInviteAsync(Invite invite);
+        public Task<bool> AnyInviteAsync(Guid token, string email, int companyId);
+        public Task<Invite> GetInviteAsync(int inviteId, int companyId);
+        public Task<Invite> GetInviteAsync(Guid token, string email, int companyId);
+        public Task<bool> ValidateinviteCodeAsync(Guid? token);
+    }
+}
