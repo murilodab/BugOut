7b065964fac0a06e29da1a223cb54e687c035eda
Murilo Barbosa <muriloab96@gmail.com>
2023-12-14 10:22:38 -0300

    created interface for AppNotificationsService

---
+++ Services/Interfaces/IAppNotificationService.cs
@@ -0,0 +1,14 @@
+﻿using BugOut.Models;
+
+namespace BugOut.Services.Interfaces
+{
+    public interface IAppNotificationService
+    {
+        public Task AddNotificationAsync(Notification notification);
+        public Task<List<Notification>> GetReceivedNotificationsAsync(string userId);
+        public Task<List<Notification>> GetSentNotificationsAsync(string userId);
+        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role);
+        public Task SendMembersEmailNotificationsAsync(Notification notification, List<AppUser> members);
+        public Task SendEmailNotificationsAsync(Notification notification, string emailSubject);
+    }
+}
