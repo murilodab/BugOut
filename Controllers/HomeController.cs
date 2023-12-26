c9cc10f1c54afc1d5343b26ce96f56195725f6df
Murilo Barbosa <muriloab96@gmail.com>
2023-12-26 19:10:31 -0300

    dashboard action created in HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using BugOut.Models;
+﻿using BugOut.Extensions;
+using BugOut.Models;
+using BugOut.Models.ViewModels;
+using BugOut.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +11,30 @@ namespace BugOut.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IAppCompanyInfoService _companyInfoService;
+        private readonly IAppProjectService _projectService;
+        private readonly IAppTicketService _ticketService;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IAppCompanyInfoService companyInfoService, IAppProjectService projectService, IAppTicketService ticketService, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _companyInfoService = companyInfoService;
+            
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            DashBoardViewModel model = new();
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
+            model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets).Where(t => t.Archived == false).ToList();
+            model.Members = model.Company.Members.ToList();
+
+
+            return View(model);
         }
 
         public IActionResult Index()
---
+++ Models/ViewModels/DashBoardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+
+namespace BugOut.Models.ViewModels
+{
+    public class DashBoardViewModel
+    {
+        public  Company Company { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<AppUser> Members { get; set; }
+    }
+}
