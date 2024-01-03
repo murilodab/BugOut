26c8bcd6df62ca5a3f02dbf77e33e072135a4e59
Murilo Barbosa <muriloab96@gmail.com>
2024-01-03 20:17:20 -0300

    Added ViewModel for Comapny Info

--- Controllers/CompaniesController.cs
+++ Controllers/CompaniesController.cs
@@ -7,16 +7,22 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BugOut.Data;
 using BugOut.Models;
+using BugOut.Services.Interfaces;
+using BugOut.Models.ViewModels;
+using BugOut.Extensions;
 
 namespace BugOut.Controllers
 {
     public class CompaniesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IAppCompanyInfoService _companyInfoService;
 
-        public CompaniesController(ApplicationDbContext context)
+
+        public CompaniesController(ApplicationDbContext context, IAppCompanyInfoService companyInfoService)
         {
             _context = context;
+            _companyInfoService = companyInfoService;
         }
 
         // GET: Companies
@@ -25,6 +31,30 @@ namespace BugOut.Controllers
               return _context.Companies != null ? 
                           View(await _context.Companies.ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Companies'  is null.");
+        }
+
+
+        public async Task<IActionResult> CompanyInfo()
+        {
+            try
+            {
+                CompanyInfoViewModel model = new();
+
+                int companyId = User.Identity.GetCompanyId().Value;
+
+                model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
+                model.Members = model.Company.Members.ToList();
+
+                return View(model);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return NotFound();
+
         }
 
         // GET: Companies/Details/5
---
+++ Models/ViewModels/CompanyInfoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BugOut.Models.ViewModels
+{
+    public class CompanyInfoViewModel
+    {
+        public Company Company { get; set; }
+        public List<AppUser> Members { get; set; }
+
+    }
+}
